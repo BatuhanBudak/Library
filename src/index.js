0c4f16f24f04b78bbc6356f3ef26a4462bff9e2c
Batuhan <batuhan.budak@gmail.com>
2022-04-28 00:07:43 +0300

    Add firebaseui-web

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-import { initializeApp } from "firebase/app";
-import './style.css'
+import './style.css' 
+import {firebaseConfigStart} from './firebase-config';
+// import {uiConfigStart} from './ui-config';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAmk93N7QbB7OcQHjRjJd_ZaQ1wDxiAePg",
-
-  authDomain: "library-project-5c6ff.firebaseapp.com",
-
-  projectId: "library-project-5c6ff",
-
-  storageBucket: "library-project-5c6ff.appspot.com",
-
-  messagingSenderId: "966224722696",
-
-  appId: "1:966224722696:web:53029a977858233d04cabd",
-};
-
+firebaseConfigStart();
+// uiConfigStart();
 let myLibrary = [];
 
 class Book {
@@ -111,5 +100,5 @@ function toggleReadValue(e) {
 function findBookIdByTitle(title) {
   return myLibrary.findIndex((book) => book.title === title);
 }
+  
 
-const app = initializeApp(firebaseConfig);
