d935d04ede31a76f4cc8f7193b3aad3580295a33
Batuhan <batuhan.budak@gmail.com>
2022-04-27 14:17:14 +0300

    Configure webpack before firebase setup

---
+++ src/script.js
@@ -0,0 +1,115 @@
+import { initializeApp } from "firebase/app";
+import './style.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmk93N7QbB7OcQHjRjJd_ZaQ1wDxiAePg",
+
+  authDomain: "library-project-5c6ff.firebaseapp.com",
+
+  projectId: "library-project-5c6ff",
+
+  storageBucket: "library-project-5c6ff.appspot.com",
+
+  messagingSenderId: "966224722696",
+
+  appId: "1:966224722696:web:53029a977858233d04cabd",
+};
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages + " pages";
+    this.read = read;
+  }
+}
+
+const formContainer = document.getElementsByClassName("form-popup");
+const addBookButton = document.getElementById("new-book-button");
+const cancelButton = document.getElementById("cancel");
+const submitButton = document.querySelector("#submit");
+const form = document.querySelector("#form-container");
+const cardContainer = document.querySelector(".card-container");
+document.onkeydown = handleKeyboardInput;
+
+addBookButton.onclick = () => (formContainer[0].style.display = "block");
+cancelButton.onclick = () => (formContainer[0].style.display = "none");
+cancelButton.onkey;
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  if (e.submitter.id === "cancel") return;
+  e.preventDefault();
+
+  const newBook = new Book(
+    form.title.value,
+    form.author.value,
+    form.pages.value,
+    form.read.checked
+  );
+
+  myLibrary.push(newBook);
+
+  createBookCard(newBook);
+
+  formContainer[0].style.display = "none";
+  form.reset();
+}
+
+function createBookCard(newBook) {
+  const newCard = document.createElement("div");
+
+  newCard.setAttribute("id", findBookIdByTitle(newBook.title));
+  newCard.classList.add("card");
+
+  const titleHeader = document.createElement("h3");
+  const authorHeader = document.createElement("h3");
+  const pagesHeader = document.createElement("h3");
+  const cardDeleteButton = document.createElement("button");
+  const cardReadButton = document.createElement("button");
+
+  titleHeader.textContent = newBook.title;
+  authorHeader.textContent = newBook.author;
+  pagesHeader.textContent = newBook.pages;
+  cardReadButton.textContent = newBook.read.checked ? `Read` : `Not read`;
+  cardDeleteButton.textContent = "Remove";
+
+  newCard.appendChild(titleHeader);
+  newCard.appendChild(authorHeader);
+  newCard.appendChild(pagesHeader);
+  newCard.appendChild(cardDeleteButton);
+  newCard.appendChild(cardReadButton);
+  newCard.appendChild(cardDeleteButton);
+  newCard.appendChild(cardReadButton);
+  cardContainer.appendChild(newCard);
+
+  cardReadButton.addEventListener("click", toggleReadValue);
+  cardDeleteButton.addEventListener("click", removeCard);
+}
+
+function handleKeyboardInput(e) {
+  if (e.key === "Escape") {
+    formContainer[0].style.display = "none";
+  }
+}
+function removeCard(e) {
+  const parentNode = e.target.parentNode;
+  myLibrary.splice(parentNode.id, 1);
+  parentNode.remove();
+}
+
+function toggleReadValue(e) {
+  const parentNodeId = e.target.parentNode.id;
+  myLibrary[parentNodeId].read
+    ? (e.target.textContent = "Not Read")
+    : (e.target.textContent = "Read");
+  myLibrary[parentNodeId].read = !myLibrary[parentNodeId].read;
+}
+
+function findBookIdByTitle(title) {
+  return myLibrary.findIndex((book) => book.title === title);
+}
+
+const app = initializeApp(firebaseConfig);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/script.js',
+  devtool: 'inline-source-map',
+  plugins: [
+
+    new HtmlWebpackPlugin({
+
+      title: 'Development',
+      template: 'src/entry.html'
+    }),
+
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+
+    rules: [
+
+      {
+
+        test: /\.css$/i,
+
+        use: ['style-loader', 'css-loader'],
+
+      },
+
+    ],
+
+  },
+};
